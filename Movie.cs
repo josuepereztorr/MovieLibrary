94d54cce20e4a72ed702fc9385a8a3928011dd49
Josue <josuepereztorr@gmail.com>
2022-02-19 14:09:41 -0600

    updated Movie.cs by adding getters and setters to get the three main properties to be able to parse the csv.

--- Movie.cs
+++ Movie.cs
@@ -2,17 +2,35 @@ using System;
 using System.Collections.Generic;
 
 class Movie {
+
     public int MovieId {get; set;}
-    public string Title {get; set;} 
-    public int Year {get; set;}
+    private string title;
+    private int year;
     private List<string> genres = new List<string>();
 
-    public List<string> getGenres() {
-        return genres;
+    public Movie(int movieId, string title, int year, string genres) {
+
+        this.MovieId = movieId;
+        this.title = title;
+        this.year = year;
+        addGenre(genres);
     }
 
-    public void addGenre(string genre) {
-        genres.Add(genre);
+    public string getTitle() {
+        return $"{title} ({year})";
+    }
+
+    public string getGenres() {
+        return String.Join("|", this.genres);
+    }
+
+    private void addGenre(string genre) {
+
+        string[] genreArr = genre.Split(",");
+
+        foreach (string g in genreArr) {
+            this.genres.Add(g);
+        }
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ namespace MovieLibrary
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
